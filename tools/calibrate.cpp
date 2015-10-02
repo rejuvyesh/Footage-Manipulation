d6dcc1db1ca98a1916962d86fa0380341f1055ce
rejuvyesh <mail@rejuvyesh.com>
2015-10-01 18:14:30 -0700

    opencv 3 headers

--- tools/calibrate.cpp
+++ tools/calibrate.cpp
@@ -1,10 +1,10 @@
 // OpenCV includes
-#include "opencv2/features2d/features2d.hpp"
-#include "opencv2/core/core.hpp"
-#include "opencv2/calib3d/calib3d.hpp"
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include "opencv2/contrib/contrib.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/calib3d.hpp"
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+// #include "opencv2/contrib.hpp"
 
 // Boost includes
 #include <boost/program_options.hpp>
